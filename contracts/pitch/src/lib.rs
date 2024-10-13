a27c23b34643a7e4e47af108c594543c5d5b6c8b
A7 <danieladeniyi17@outlook.com>
2024-10-13 01:01:43 +0100

    Pitch CRUD Contract

--- contracts/pitch/src/lib.rs
+++ contracts/pitch/src/lib.rs
@@ -1,16 +1,6 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, String, Symbol};
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, String, Symbol, Vec};
 
-#[contracttype]
-pub enum DataKey {
-  Creator(Address),
-  Seller(Address),
-  Token(Address),
-  Product(Address),
-  TimeToLive(i32),
-  Ratio(i32),
-  Description(String),
-}
 #[contracttype]
 pub struct PitchData {
   creator: Address,
@@ -22,16 +12,38 @@ pub struct PitchData {
   description: String
 }
 
-const PITCH_INFO: Symbol = symbol_short!("PitchInfo");
+impl PitchData {
+  pub fn conv_to_vec() {
+
+  }
+}
+
+const DATA: Symbol = symbol_short!("DATA");
 
 #[contract]
 pub struct CreatePitch;
 
 #[contractimpl]
 impl CreatePitch {
-  pub fn create(env: Env, user: Address, prospect: Address, product: Address, token: Address, time_to_live: i32, ratio: i32, description: String) {
+  pub fn create_pitch(env: Env, user: Address, prospect: Address, product: Address, token: Address, time_to_live: i32, ratio: i32, description: String) {
     user.require_auth();
     let data: PitchData = PitchData{creator: user, seller: prospect, token: token, product: product, time_to_live: time_to_live, ratio: ratio, description: description};
-    env.storage().instance().set(&PITCH_INFO, &data);
+    env.storage().instance().set(&DATA, &data);
+  }
+
+  pub fn get_pitch(env: Env, user: Address) -> Option<PitchData> {
+    user.require_auth();
+    if env.storage().instance().has(&DATA) {
+      return Some(env.storage().instance().get(&DATA).unwrap());
+    } else {
+      None
+    }
+    
+  }
+
+  pub fn update_pitch(env: Env, user: Address) {
+    user.require_auth();
+    
+  
   }
 }
